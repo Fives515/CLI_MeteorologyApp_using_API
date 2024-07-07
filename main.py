f45e1bc308c17c6c841e204b8e9cd1bc63984943
afonso <afonsoesanto@gmail.com>
2024-07-07 21:59:11 +0100

    Displays a graph with temperature needs to be flexible with time

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import datetime
+from datetime import *
 from astral import LocationInfo
 from astral.sun import sun
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
 
-x=datetime.datetime.now()
-day=(x.strftime("%d"))
-month=(x.strftime("%m"))
-year = datetime.datetime.now()
-print(year)
+
+time = datetime.now()
+end = datetime(2024, 7, 31)
 city = LocationInfo("London","England", "Europe/London", 51.43626662318104, -0.05197242930741357)
-s=sun(city.observer, date=year)
+location = Point(51.43626662318104, -0.05197242930741357)
+s=sun(city.observer, date=time)
+data = Daily(location, time, end)
+data=data.fetch()
+data.plot(y=["tavg", "tmin", "tmax"])
+plt.show()
+print(data)
 
 print(city.name, city.timezone)
 print((
