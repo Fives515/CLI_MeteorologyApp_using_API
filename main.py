d239e8cdec8e7892b57f2f74d421671b66997d37
Afonso <afonsoesanto@gmail.com>
2024-07-20 19:29:38 +0100

    Working code the direction if statment give the correct string

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def extract_time(datetime_string):
 # Get the current date
 current_date = datetime.now()
 
+
 # Generate and print dates for the next 7 days with average temperatures
 end_date = (current_date + timedelta(days=6)).strftime("%Y-%m-%d")   # week range for weekly data for each day
 start_date = current_date.strftime('%Y-%m-%d')
@@ -45,8 +46,29 @@ print(f"City: {city}")
 # Retry mechanism for fetching temperature data
 daily_data = get_daily_temperature(latitude, longitude, start_date, end_date)
 
+def direction (wind_direction):
+    if wind_direction == 0 or wind_direction == 360:
+        return "North"
+    elif wind_direction == 90:
+        return "East"
+    elif wind_direction == 180:
+        return "South"
+    elif wind_direction == 270:
+        return "West"
+    elif wind_direction > 0 or wind_direction < 90:
+        return "NE"
+    elif wind_direction > 90 or wind_direction < 180:
+        return "SE"
+    elif wind_direction > 180 or wind_direction < 270:
+        return "SW"
+    elif wind_direction > 270 or wind_direction < 360:
+        return "NW"
+    else:   
+        return "gay"
+
 if daily_data.get("data"):
     for day_data in daily_data["data"]:
+        print (day_data)
         date = day_data.get("date")
         date_obj = datetime.strptime(date, '%Y-%m-%d')
         formatted_date = date_obj.strftime('%d/%m/%Y')
@@ -54,13 +76,14 @@ if daily_data.get("data"):
         max_temp = day_data.get("tmax")
         min_temp = day_data.get("tmin")
         wind_gust = day_data.get("wpgt")
-        wind_direction = day_data.get ("wdir")
         wind_speed = day_data.get("wspd")
         sea_level_air_pressure = day_data.get("pres")
         abd = datetime.date(date_obj)
         sunrise= sun.get_local_sunrise_time(abd)
         sunset = sun.get_local_sunset_time(abd)
         
+        wind_int =direction(day_data.get ("wdir"))
+        print (wind_int)
         #Extract the time and calculate it
         datetime_string = str(sunrise)
         time_sunrise = extract_time(datetime_string)
@@ -76,7 +99,7 @@ if daily_data.get("data"):
             print(f"Minimum Temperature: {min_temp}°C")
             print(f"Maximum Temperature: {max_temp}°C")
             print(f"Maximum wind gust: {wind_gust} km/h")
-            print(f"The wind will be coming from {wind_direction}° with an average wind speed: {wind_speed} km/h")
+            print(f"The wind will be coming from {wind_int} with an average wind speed: {wind_speed} km/h")
             print(f"The sun will rise at {time_sunrise} and set at {time_sunset} which will give you {tdelta} suntime")
             print(f"Percepitation: {precipitation} mm\n")
         else:
